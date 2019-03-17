ca53aa85e655c6c262ffc22176f555f3eae49b81
Fousiya M P <mpfouziya@gmail.com>
2019-03-17 21:53:48 +0400

    Serve class change

--- Serve.java
+++ Serve.java
@@ -5,16 +5,14 @@ import java.util.Map;
 import java.util.Queue;
 
 public class Serve {
-	
 
 	public static LinkedList<Order> currentQueueOrder=new LinkedList<Order>();
 	public static LinkedList<Order> currentCookOrder=new LinkedList<Order>();
+	
 	public static Queue<String> currentQueue=new LinkedList<>();
-	static final Object lock=new Object();
-	static String tookOrder=null;
-	static String cookFinish=null;
+	public static Queue<String> currentCookingQueue=new LinkedList<>();
 	
-	
+	//Get first order in the queue
 	public LinkedList<Order> getFirstOrder(LinkedList<Order> currentOrder) {
 		LinkedList<Order> currOrder= new LinkedList<Order>();
 		try
@@ -53,6 +51,7 @@ public class Serve {
 		return milliSec;
 	}
 	
+	//Get total milliseconds of an order
 	public int getTotalMilliSec(LinkedList<Order> orderlist) {
 		int totalMilliSec=0;
 		for(Order o: orderlist){
@@ -60,6 +59,8 @@ public class Serve {
 		}
 		return totalMilliSec;
 	}
+	
+	//Get Food Name by Item Id
 	public String getNameByItemId(String itemId) {
 		String name=null;
 		for(Map.Entry<String, Item> entry: ProcessClass.itemlist.entrySet()){
@@ -69,10 +70,13 @@ public class Serve {
 		}
 		return name;
 	}
+	
+	//Cook Start Cooking
 	public void startCook()
 	{
 		int MilliSec;
-		String currCust;
+		String currCust;	
+
 		try
 		{
 			synchronized(Serve.currentCookOrder)
@@ -83,11 +87,13 @@ public class Serve {
 		        currCust=currOrder.getFirst().getCustId();
 		        System.out.println(Thread.currentThread().getName()+" Currently Started Cooking for:" +currCust+".Total time:"+MilliSec+" min");
 		        
-		        Serve.currentCookOrder.removeAll(currOrder);
-		        
+		        //Remove order from cooking queue
+		        Serve.currentCookOrder.removeAll(currOrder);       
 		        Serve.currentCookOrder.notifyAll();	
 		      
 			}
+			
+			//Time taken for cooking according to the food ordered
 			for(int i=MilliSec;i>=0;i=i-1)
 			{
 				try{Thread.sleep(1000);} catch(Exception e){}
@@ -96,10 +102,16 @@ public class Serve {
 			System.out.println(Thread.currentThread().getName()+" Finished Cooking for:" +currCust);
 			Thread.sleep(1000);
 			System.out.println(currCust+" Recieved Food Ordered");
+			
+			//Remove customer from cooking queue after finishing cooking
+			Serve.currentCookingQueue.remove();
+			System.out.println("Cooking to be finished for:"+Serve.currentCookingQueue);
 		
 		} 
 		catch(Exception e) {}
 		}
+	
+	//Waiter Start Servicing
 	public void serviceOrder()
 	{
 		int MilliSec;
@@ -113,15 +125,23 @@ public class Serve {
 		        LinkedList<Order>currOrder =getFirstOrder(Serve.currentQueueOrder);
 		        MilliSec=getTotalMilliSec(currOrder)/1000;
 		        currCust=currOrder.getFirst().getCustId();
-		        System.out.println(Serve.currentQueue);
+		        System.out.println("Current waiting queue:"+Serve.currentQueue);
 		        System.out.println("Current First Order:" +currCust);
 		        System.out.println(Thread.currentThread().getName()+" Currently Taking Order from:" +currCust+".Total time:"+MilliSec+" min");
+		       
+		        //Getting the food ordered by the customer
 		        System.out.println("Food ordered are:");
 		        for(Order o:currOrder)
 		        {
 		        	System.out.println(getNameByItemId(o.getItemId())+" "+o.getQuantity());
 		        }
+		        
+		        //Adding order to cooking queue
 		        Serve.currentCookOrder.addAll(currOrder);
+		        Serve.currentCookingQueue.add(currCust);
+		        System.out.println("Cooking to be finished for:"+Serve.currentCookingQueue);
+		        
+		        //Removing order from current waiting queue once took order
 		        Serve.currentQueueOrder.removeAll(currOrder);
 		        Serve.currentQueue.remove(currOrder.get(0).getCustId());
 		        Serve.currentQueueOrder.notifyAll();		        		    
@@ -129,7 +149,9 @@ public class Serve {
 			}
 
 		} 
-	catch(Exception e) {}		
+	catch(Exception e) {}	
+		
 	}
+	
 
 }
